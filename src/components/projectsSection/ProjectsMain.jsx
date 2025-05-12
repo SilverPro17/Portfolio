2377646e3ed5f9a83601581227d285261cff826b
SilverPro17 <rodriguessilvano242@gmail.com>
2025-05-12 11:47:04 +0000

    Updating

--- src/components/projectsSection/ProjectsMain.jsx
+++ src/components/projectsSection/ProjectsMain.jsx
@@ -47,6 +47,7 @@ const ProjectsMain = () => {
               year={project.year}
               align={project.align}
               image={project.image}
+              link={project.link} 
             />
           );
         })}
